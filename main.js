a15958fef80bfbac2cde697c6fab4cc69b04532f
AlexandrCh04 <a.chertikhin0072@mail.ru>
2021-02-20 23:54:12 +0500

    .

--- main.js
+++ main.js
@@ -13,8 +13,17 @@ link.addEventListener('click', function (e) {
 const modalContent = document.createElement('div');
 modalContent.classList.add('modal-content');
 myModal.append(modalContent);
-const video = `<video src="${url}" class="modal-video" autoplay muted preload="auto"></video>`
+const video = `<video src="${url}" class="modal-video" autoplay muted="muted" preload="auto"></video>`
 modalContent.insertAdjacentHTML('beforeend', video);
+const closeLink = document.createElement('a');
+closeLink.classList.add('close');
+closeLink.textContent = 'x';
+closeLink.setAttribute('href', '#');
+myModal.append(closeLink);
+closeLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    myModal.remove();
+})
 
 window.addEventListener('click', function (e) {
     if (e.target == myModal) {
