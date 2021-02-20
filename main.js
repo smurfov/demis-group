5ef73c8999b5bd787ebd3743f43db33435989ac4
AlexandrCh04 <a.chertikhin0072@mail.ru>
2021-02-20 23:35:36 +0500

    .

---
+++ main.js
@@ -0,0 +1,23 @@
+const url = "video.mp4";
+const link = document.querySelector('#link');
+const body = document.querySelector('body');
+
+const myModal = document.createElement('div');
+myModal.classList.add('modal-back');
+
+link.addEventListener('click', function (e) {
+    e.preventDefault();
+    body.append(myModal);
+})
+
+const modalContent = document.createElement('div');
+modalContent.classList.add('modal-content');
+myModal.append(modalContent);
+const video = `<video src="${url}" class="modal-video" autoplay muted preload="auto"></video>`
+modalContent.insertAdjacentHTML('beforeend', video);
+
+window.addEventListener('click', function (e) {
+    if (e.target == myModal) {
+        myModal.remove();
+    }
+})
